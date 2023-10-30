3bd43148d671743c5db249e247783f56739260d6
huangyan05 <huangyan05@bilibili.com>
2023-10-30 19:47:49 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/remotehy/go-std-mod-b
+
+go 1.21
+
+require github.com/remotehy/go-std-mod v0.0.0-20231030112727-57fb5d725b19 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	s2 "github.com/remotehy/go-std-mod/lib/string"
+)
+
+func main() {
+	fmt.Println(s2.Duplicate("haha", 3, ","))
+}
